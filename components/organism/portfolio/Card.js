c8a674432e6d2ba929b7414e7517694445a1a528
AmienulRana <amienulrana@gmail.com>
2022-11-10 15:40:18 +0700

    menambahkan skills yang sudah dikuasai pada variabled tech

--- components/organism/portfolio/Card.js
+++ components/organism/portfolio/Card.js
@@ -1,39 +1,70 @@
-import styles from '../../../styles/home.module.scss';
-import { DiHtml5, DiCss3, DiBootstrap, DiReact, DiJavascript1, DiGulp } from 'react-icons/di'; 
-import { SiExpress, SiRedux, SiTailwindcss, SiMongodb, SiNextdotjs, SiNpm, SiGithub, SiGit, SiWebpack } from 'react-icons/si';
-import { FaNodeJs } from 'react-icons/fa';
-import Image from 'next/image'
+import styles from "../../../styles/home.module.scss";
+import {
+  DiHtml5,
+  DiCss3,
+  DiBootstrap,
+  DiReact,
+  DiJavascript1,
+  DiSass,
+} from "react-icons/di";
+import {
+  SiExpress,
+  SiRedux,
+  SiTailwindcss,
+  SiMongodb,
+  SiNextdotjs,
+  SiNpm,
+  SiGithub,
+  SiGit,
+  SiWebpack,
+  SiVuedotjs,
+  SiTypescript,
+} from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+import Image from "next/image";
 export const Tech = {
-    html: DiHtml5,
-    css:  DiCss3,
-    bs:   DiBootstrap,
-    js: DiJavascript1,
-    react:DiReact,
-    node: FaNodeJs,
-    express: SiExpress,
-    redux: SiRedux,
-    tailwind: SiTailwindcss,
-    mongodb: SiMongodb,
-    next:SiNextdotjs,
-    npm: SiNpm,
-    github: SiGithub,
-    git: SiGit,
-    gulp:DiGulp,
-    webpack:SiWebpack
-}
-function Card({image, title, tech, animation}) {
-    const RenderTech = tech.map(item => Tech[item]);
+  html: DiHtml5,
+  css: DiCss3,
+  bs: DiBootstrap,
+  js: DiJavascript1,
+  react: DiReact,
+  node: FaNodeJs,
+  express: SiExpress,
+  redux: SiRedux,
+  tailwind: SiTailwindcss,
+  mongodb: SiMongodb,
+  next: SiNextdotjs,
+  npm: SiNpm,
+  github: SiGithub,
+  git: SiGit,
+  vuejs: SiVuedotjs,
+  typesript: SiTypescript,
+  sass: DiSass,
+  webpack: SiWebpack,
+};
+function Card({ image, title, tech, animation }) {
+  const RenderTech = tech.map((item) => Tech[item]);
   return (
     <>
-        <figure>
-            <Image src={image} placeholder="blur" width={300} height={200} data-aos="fade-up" data-aos-duration={animation} alt={title}/>
-            <figcaption>{title}</figcaption>
-        </figure>
-        <div className={styles.Tech}>
-            {RenderTech.map((Item, i) => <Item  key={i} />)}
-        </div>
+      <figure>
+        <Image
+          src={image}
+          placeholder="blur"
+          width={300}
+          height={200}
+          data-aos="fade-up"
+          data-aos-duration={animation}
+          alt={title}
+        />
+        <figcaption>{title}</figcaption>
+      </figure>
+      <div className={styles.Tech}>
+        {RenderTech.map((Item, i) => (
+          <Item key={i} />
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default Card
+export default Card;
