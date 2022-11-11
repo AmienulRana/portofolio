7c3a44bec0d341ef4d0045c533865f0766967342
AmienulRana <amienulrana@gmail.com>
2022-11-11 10:15:48 +0700

    menambahkan marketplace sebagai portofolio baru

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -6,6 +6,7 @@ import LandingPageProperty from '../assets/landing page property.png';
 import InvitationsDigital from '../assets/undangan digital.png';
 import TopupGame from '../assets/topupgame.png';
 import Card from './organism/portfolio/Card';
+import Marketplace from '../assets/marketplace.jpg';
 
 export default function Portfolio() {
   return (
@@ -15,19 +16,22 @@ export default function Portfolio() {
         <hr />
         <div style={{display: 'flex', flexWrap: 'wrap'}}>
             <div className={styles.WrapperImg}>
-                <Card animation="300" image={HefaStore} title="online shop(static)" tech={['html', 'css', 'bs']}/>
+                <Card animation="300" image={HefaStore} title="Online shop(static)" tech={['html', 'css', 'bs']}/>
             </div>
             <div className={styles.WrapperImg}>
-                <Card animation="300" image={LandingPage} title="landing page healty food" tech={['html', 'css', 'js','bs']}/>
+                <Card animation="300" image={LandingPage} title="Landing page healty food" tech={['html', 'css', 'js','bs']}/>
             </div>
             <div className={styles.WrapperImg}>
-                <Card animation="500" image={LandingPageProperty} title="landing page property" tech={['html', 'css', 'js','tailwind']}/>
+                <Card animation="500" image={LandingPageProperty} title="Landing page property" tech={['html', 'css', 'js','tailwind']}/>
             </div>
             <div className={styles.WrapperImg}>
-                <Card animation="500" image={InvitationsDigital} title="undangan berbasis QrCode" tech={['css', 'react','mongodb', 'express', 'node']}/>
+                <Card animation="500" image={InvitationsDigital} title="Undangan berbasis QrCode" tech={['css', 'react','mongodb', 'express', 'node']}/>
             </div>
             <div className={styles.WrapperImg}>
                 <Card animation="700" image={TopupGame} title="Website topup game" tech={['css', 'typesript','mongodb', 'express', 'node']}/>
+            </div>
+            <div className={styles.WrapperImg}>
+                <Card animation="700" image={Marketplace} title="Marketplace" tech={['vuejs', 'tailwind', 'mongodb', 'express', 'node']}/>
             </div>
         </div>
     </div>
