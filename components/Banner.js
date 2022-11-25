92db2fe94fa0680693629e83fbe00c341e61cea5
AmienulRana <amienulrana@gmail.com>
2022-11-25 20:01:05 +0700

    menghapus kata highly pada banner

--- components/Banner.js
+++ components/Banner.js
@@ -12,8 +12,8 @@ export default function Banner(){
                 </a>
             </div>
             <h1 data-aos="fade-up" data-aos-duration="600">Hello, i&apos;am Amienul Rana</h1>
-            <h2 data-aos="fade-up" data-aos-duration="800">Freelance frontend developer based in Medan, Indonesia.
-                Highly experienced in maintenance & developing websites.</h2>
+            <h2 data-aos="fade-up" data-aos-duration="800">Freelance Frontend developer based in Medan, Indonesia.
+                Experienced in maintenance & developing websites.</h2>
             <p data-aos="fade-up" data-aos-duration="1000">I am a Front end developer who has more than 1 year of experience and also has good coding skills in creating or developing applications with high performance and neat architecture. I also have experience in Back end developer position.</p>
         </div>
     )
